c1ef5502dfca3ce23486e8e39430d619df326605
Nicpanel <nico.panel@gmail.com>
2019-07-03 23:09:34 +0200

    Deploy using FTP as SFTP is not available /!\ Unsecure!

--- src/deploy.js
+++ src/deploy.js
@@ -1,69 +1,25 @@
 #!/usr/bin/env node
-const fs = require('fs');
-// const Client = require('ssh2').Client;
-const glob = require('glob');
 
 const basePath = './dist';
 const destinationPath = '/www/zerowasteuppsala.org/map/';
+
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
 const config = {
   host: process.env.FTP_HOST,
   password: process.env.FTP_PASSWORD,
-  username: process.env.FTP_USER,
+  user: process.env.FTP_USER,
+  port:21,
+  localRoot: './dist',
+  remoteRoot: '.',
+  include: ['*', '**/*'],
+  deleteRemote: false,
+  forcePasv: true
 };
 
-var numberOfOpLaunched = 0;
-var numberOfOpTerminated = 0;
+// use with promises
+ftpDeploy.deploy(config)
+    .then(res => console.log('finished:', res))
+    .catch(err => console.log(err))
 
-let Client = require('ssh2-sftp-client');
-let sftp = new Client();
-
-// Check if the path is a directory and
-// either create the directory on the server
-// if it is a directory, or upload the file
-// if it is a file.
-function handlePath(path) {
-  const relativeFile = path.replace(`${basePath}/`, '');
-  const destination = `${destinationPath}/${relativeFile}`;
-
-  if (fs.lstatSync(path).isDirectory()) {
-    return createDirectory(destination);
-  }
-
-  return uploadFile(path, destination);
-}
-
-function createDirectory(destination) {
-  console.log(`createDirectory=${destination}`);
-  numberOfOpLaunched++;
-  return sftp.mkdir(destination).then(() => {
-    manageTerminatedOperation();
-  }).catch((err) => {
-    manageTerminatedOperation();
-  });
-}
-
-function uploadFile(file, destination) {
-  console.log(`uploadFile ${file}`);
-  numberOfOpLaunched++;
-  return sftp.put(file, destination).then(() => {
-    manageTerminatedOperation();
-  }).catch((err) => {
-    manageTerminatedOperation();
-  });
-}
-
-function manageTerminatedOperation() {
-  numberOfOpTerminated++;
-  if (numberOfOpTerminated >= numberOfOpLaunched) {
-    console.log(`Process terminated with ${numberOfOpTerminated}`);
-    sftp.end();
-  } else {
-    console.log(`Op terminated ; numberOfOpTerminated=${numberOfOpTerminated} ; numberOfOpLaunched=${numberOfOpLaunched}`);
-  }
-}
-
-sftp.connect(config).then(() => {
-    glob.sync(`${basePath}/**/*`).forEach(handlePath);
-}).catch((err) => {
-    console.log(err, 'catch error');
-});
